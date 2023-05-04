5aade3bcf2c82acb0773c7ae47acff58a4260966
robinschmid <robinschmid@uni-muenster.de>
2023-05-04 10:24:53 +0200

    combined metadata MASST

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import views
 
 import dash_masstplus
 import dash_microbemasst
+import dash_foodmasst2
+import dash_plantmasst
+import dash_metadatamasst
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='5000')
