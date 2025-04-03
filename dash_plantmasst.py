1a29d0a88f6f7916f2a7bacfe8c962887b6da63c
Wilhan Nunes <85765131+wilhan-nunes@users.noreply.github.com>
2025-04-03 11:05:55 -0700

    Bump version

--- dash_plantmasst.py
+++ dash_plantmasst.py
@@ -60,7 +60,7 @@ NAVBAR = dbc.Navbar(
         ),
         dbc.Nav(
             [
-                dbc.NavItem(dbc.NavLink("plantMASST Dashboard - Version 2025.03.25", href="/plantmasst")),
+                dbc.NavItem(dbc.NavLink("plantMASST Dashboard - Version 2025.04.03", href="/plantmasst")),
             ],
         navbar=True)
     ],
